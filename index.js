467bde5c13ad694aeaabd92da9b632b01dcc647f
ruiehq <seocored@gmail.com>
2017-11-16 00:01:20 +0800

    Inital commit to heroku

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+   res.send({ greet: 'hi' });
+});
+
 app.listen(PORT, () => {
    console.log(`Server is running on PORT: ${PORT}`);
 });
