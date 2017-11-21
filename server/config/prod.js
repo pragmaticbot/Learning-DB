022f687e437dd80ca7e67a41eb8be500155f1629
ruiehq <seocored@gmail.com>
2017-11-21 18:49:14 +0800

    Added cookieKey property in prod.js

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,5 +1,6 @@
 module.exports = {
    googleID: process.env.GOOGLE_ID,
    googleSecret: process.env.GOOGLE_SECRET,
-   mlabURL: process.env.MLAB_URL
+   mlabURL: process.env.MLAB_URL,
+   cookieKey: process.env.COOKIE_KEY
 };
