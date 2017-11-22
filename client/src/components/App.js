9c4145005f02e0fb916df30c85fdd96983f46eb1
ruiehq <seocored@gmail.com>
2017-11-22 21:27:54 +0800

    Added reducers, provider with store in index.js client

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,1 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Header = () => { return <div>Header</div> };
+const Landing = () => { return <div>Landing</div> };
+const Dashboard = () => { return <div>Dashboard</div> };
+const Survey = () => { return <div>Survey</div> };
+const SurveyNew = () => { return <div>New Survey</div> };
+
+const App = () => {
+   return (
+      <div>
+         <BrowserRouter>
+            <div>
+               <Header />
+               <Route exact path={'/'} component={Landing} />
+               <Route exact path={'/dashboard'} component={Dashboard} />
+               <Route exact path={'/survey'} component={Survey} />
+               <Route path={'/survey/new'} component={SurveyNew} />
+            </div>
+         </BrowserRouter>
+      </div>
+   )
+};
+
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,20 @@
-import React from 'react';
-import ReactDOM from 'ReactDOM';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 
+import App from './components/App';
+import reducers from './reducers';
 
+const store = createStore(reducers, {}, applyMiddleware());
+
+class Main extends Component {
+   render() {
+      return (
+         <Provider store={store}>
+            <App />
+         </Provider>
+      )
+   }
+}
+ReactDOM.render(<Main />, document.getElementById('root'));
