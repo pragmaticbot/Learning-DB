4734d26ea69ac3d07e3615a0d23341ba9cfa3ae5
ruiehq <seocored@gmail.com>
2017-11-27 00:00:04 +0800

    Added react-stripe-checkout npm

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -23,7 +23,7 @@ class Header extends Component {
       return (
       <nav>
          <div className="nav-wrapper">
-            <Link to={this.props.auth ? '/surveysa' : '/'} className="brand-logo">Emaily</Link>
+            <Link to={this.props.auth ? '/survey' : '/'} className="brand-logo">Emaily</Link>
          <ul id="nav-mobile" className="right hide-on-med-and-down">
             {this.renderLink()}
          </ul>
