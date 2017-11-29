d429cf8f89b14f99d6eaf6ea27126d82063b2960
ruiehq <seocored@gmail.com>
2017-11-29 21:36:37 +0800

    Added semicolons server index

--- server/index.js
+++ server/index.js
@@ -36,16 +36,16 @@ app.get('/auth/google/callback', passport.authenticate('google'), (req, res) => 
 
 app.get('/surveys', (req, res) => {
    res.send('surveys');
-})
+});
 
 app.get('/api/user', (req, res) => {
    res.send(req.user);
-})
+});
 
 app.get('/api/logout', (req, res) => {
    req.logout();
    res.redirect('/');
-})
+});
 
 
 /* Run Server */
